874f5168e57411a0597fc5b28a4caed65813bb29
Karolis Zelvys <koralbish@gmail.com>
2021-10-20 14:30:07 +0300

    Added roles, role management and modal

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -36,6 +36,9 @@ export class AccountService {
   }
 
   setCurrentUser(user: User | undefined){
+    user!.roles = [];
+    const roles = this.getDecodedToken(user!.token).role;
+    Array.isArray(roles) ? user!.roles = roles : user?.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -43,6 +46,10 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(undefined);
+  }
+
+  getDecodedToken(token: string){
+    return JSON.parse(atob(token.split('.')[1]));
   }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,11 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputsComponent } from './_forms/text-inputs/text-inputs.component';
 import { DateComponent } from './_forms/date/date.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +50,12 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     PhotoEditorComponent,
     TextInputsComponent,
     DateComponent,
-    MemberMessagesComponent
+    MemberMessagesComponent,
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserModule,
