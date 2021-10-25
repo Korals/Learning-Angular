2a686abd334739838422dc47e7f94ca5a1b870c7
Karolis Zelvys <koralbish@gmail.com>
2021-10-25 13:21:17 +0300

    Pre switch to postgres

--- LCode/Startup.cs
+++ LCode/Startup.cs
@@ -73,11 +73,15 @@ namespace LCode
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "FallBack");
             });
         }
     }
